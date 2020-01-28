a4154803f75b3a7bd573a78622f4cecf7300a26e
Rahmat <akintolarahmah@gmail.com>
2020-01-28 23:35:16 +0000

    name, title, price methods added

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,38 @@
+#!/usr/bin/env ruby
+
 require 'HTTParty'
 require 'Nokogiri'
-require 'JSON'
 require 'Pry'
-require 'csv'
 
-page = HTTParty.get('https://www.annahariri.com/collections/clearance-items')
+class Watches
+    attr_reader :parse_page
+    def initialize
+        page = HTTParty.get('https://www.nike.com/w/watches-2axv8')
+        parse_page = Nokogiri::HTML(page)
+    end
 
-parse_page = Nokogiri::HTML(page)
+#method that extract and store Nike Apple watch product
+    def names
+      watch_name = []
+      parse_page.css('.product-card__link-overlay').map do |item|
+        item_container = item.text
+        watch_name << item_container
+      end
+    end
 
-Pry.start(binding)
+    def titles
+        title = []
+        parse_page.css('.product-card__subtitle').map do |item|
+            item_container = item.text unless item.css('.product-card__subtitle').nil?
+            title << item_container
+        end
+    end
+
+    def prices
+        price = []
+        parse_page.css('.css-b9fpep').map do |item|
+            item_container = item.text unless item.css('.css-b9fpep').nil?
+            price << item_container
+        end
+    end
+end
